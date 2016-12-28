4f501cd6273ea2a28c0f9d67a0842fa15a3e540a
idemoradimo <nesa.ki@live.com>
2016-12-28 09:05:15 +0100

    Edit, Add, GetAllEmployees implementirano

--- Moduo1/Moduo1/SRV1/Access/DB.cs
+++ Moduo1/Moduo1/SRV1/Access/DB.cs
@@ -27,11 +27,11 @@ namespace SRV1.Access
             }
         }
 
-        public bool AddUser(User action)
+        public bool AddUser(User user)
         {
             using (var access = new AccessDB())
             {
-                access.Actions.Add(action);
+                access.Actions.Add(user);
                 int i = access.SaveChanges();
 
                 if (i > 0)
@@ -59,7 +59,7 @@ namespace SRV1.Access
             }
         }
 
-        public bool EditUser(string username, string pass)
+        public bool EditUser(User userMain, User userEdit)
         {
             User usr = new User();
 
@@ -67,11 +67,15 @@ namespace SRV1.Access
             {
                 for (int i = 1; i <= access.Actions.Count(); i++)
                 {
-                    if (access.Actions.Find(i).Username == username)
+                    if (access.Actions.Find(i).Username == userMain.Username)
                     {
-                        if (access.Actions.Find(i).Password == pass)
+                        if (access.Actions.Find(i).Password == userMain.Password)
                         {
-                            access.Actions.Find(i).Email = "safsfda";
+                            access.Actions.Find(i).Username = userEdit.Username;
+                            access.Actions.Find(i).Password = userEdit.Password;
+                            access.Actions.Find(i).Email = userEdit.Email;
+                            access.Actions.Find(i).Role = userEdit.Role;
+
                             int k = access.SaveChanges();
 
                             if (k > 0)
@@ -83,5 +87,24 @@ namespace SRV1.Access
 
             return false;
         }
+
+        public List<User> GetAllEmployees()
+        {
+            List<User> usrs = new List<User>();
+
+            using (var access = new AccessDB())
+            {
+                for (int i = 1; i <= access.Actions.Count(); i++)
+                {
+                    if (access.Actions.Find(i).Role == Roles.Employee)
+                    {
+                        usrs.Add(access.Actions.Find(i));
+                    }
+                }
+
+                return usrs;
+            }
+        }
+
     }
 }
