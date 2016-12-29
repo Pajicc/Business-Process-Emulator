1144842c31ec4396614435d6cf3133578a458886
idemoradimo <nesa.ki@live.com>
2016-12-29 11:11:05 +0100

    modifikacijaUserKasni

--- Moduo1/Moduo1/SRV1/CompanyService.cs
+++ Moduo1/Moduo1/SRV1/CompanyService.cs
@@ -59,6 +59,7 @@ namespace SRV1
         public bool AddUser(User user)
         {
             bool done = false;
+            user.WorkTimeStart = "09:00:00";
 
             Console.WriteLine("Dodat nov User!");
             Console.WriteLine("Username: " + user.Username + "\nPassword: " + user.Password);
@@ -113,41 +114,6 @@ namespace SRV1
 
         public bool ProveriDaLiKasni(DateTime ulogovao, string timestart)
         {
-            //{0:HH:mm:ss}
-            //string ulogovao = string.Format("{0:HH:mm:ss}", DateTime.Now);
-            /*
-            string[] vreme = ulogovao.Split(':');
-            int[] intVr = new int[3];
-            int[] intVr2 = new int[3];
-
-            try
-            {
-                intVr[0] = Int32.Parse(vreme[0]);
-                intVr[1] = Int32.Parse(vreme[1]);
-                intVr[2] = Int32.Parse(vreme[2]);
-            }
-            catch (FormatException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            string[] definisanoVreme = timestart.Split(':');
-            try
-            {
-                intVr2[0] = Int32.Parse(definisanoVreme[0]);
-                intVr2[1] = Int32.Parse(definisanoVreme[1]);
-                intVr2[2] = Int32.Parse(definisanoVreme[2]);
-            }
-            catch (FormatException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            if ((intVr2[0] - intVr[0]) == 0)
-            {
-
-            }
-             * */
             DateTime definisanoVreme = Convert.ToDateTime(timestart);
 
             TimeSpan span = ulogovao.Subtract(definisanoVreme);
