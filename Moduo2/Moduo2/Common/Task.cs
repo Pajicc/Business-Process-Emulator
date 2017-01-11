31405f1b7c2c7a3d49df3de271c56a7b6277ae25
stefan <cvetkovistefan@gmail.com>
2017-01-11 09:13:57 +0100

    rewtr

---
+++ Moduo2/Moduo2/Common/Task.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Task
+    {
+        private int id;
+        private string nazivTask = string.Empty;
+
+        Task() { }
+
+        Task(string naziv)
+        {
+            this.nazivTask = naziv;
+        }
+      
+        public string NazivTask
+        {
+            get { return nazivTask; }
+            set { nazivTask = value; }
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        
+    }
+}
